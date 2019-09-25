f41bef874cdd00b3825ba7c0c345643f7455272d
Charlie <cr646531@gmail.com>
2019-09-25 15:15:01 -0400

    added 'show_hands' and 'prompt_player_action' methods

--- gameplay.py
+++ gameplay.py
@@ -34,6 +34,43 @@ def deal(deck, player, dealer):
 	player.hit(deck.pop(0))
 	player.hit(deck.pop(0))
 	dealer.hit(deck.pop(0))
+	show_hands(player, dealer)
 
+def show_hands(player, dealer):
+	print(f"---------------------------------------------------------------------\n")
 	print(f"Dealer's hand: {dealer.get_hand()}\n")
 	print(f"Player's hand: {player.get_hand()}\n")
+	print(f"---------------------------------------------------------------------\n")
+
+
+def prompt_player_action(deck, player, dealer):
+
+	response = input('Would you like to hit, stay or double-down? (h/s/d) ').lower()
+
+	if response == 'h':
+
+		player.hit(deck.pop(0))
+		show_hands(player, dealer)
+
+		if player.get_total() > 21:
+			return 1
+		else:
+			return 0
+
+	elif response == 's':
+
+		dealer.play()
+		show_hands(player, dealer)
+		return 1
+
+	elif response == 'd':
+
+		player.double_down(deck.pop(0))
+		return 1
+
+	else:
+		print("Type 'h' to hit, 's' to stay, or 'd' to double-down ")
+		return 0
+
+
+
